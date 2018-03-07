5c348b6d03ebb03a40685688c0192d64fbe31fad
David Beleza <david.beleza@mindera.com>
2018-03-07 13:09:12 +0000

    This will allow you to detect when a pushViewController animation has finished.

--- Sources/UIKit/UINavigationController.swift
+++ Sources/UIKit/UINavigationController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 extension UINavigationController {
     
-    /// Push UIViewController with a completion block
+    /// Push UIViewController with a completion block called after animation has finished
     ///
     /// - Parameters:
     ///   - viewController: viewController you want to push
     ///   - animated: animate transition
     ///   - completion: block called after animation
-    public func pushViewController(_ viewController: UIViewController, animated: Bool, completion:(()->Void)?) {
+    public func pushViewController(_ viewController: UIViewController, animated: Bool, completion:(()->(Void))?) {
         
         CATransaction.begin()
         CATransaction.setCompletionBlock(completion)
